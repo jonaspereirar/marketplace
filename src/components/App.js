0101ed10f971db13647a6617c4a77986e937795c
jonaspereirar <jonaspereirar@gmail.com>
2021-11-14 01:51:02 +0000

    feat add: Deploy

--- src/components/App.js
+++ src/components/App.js
@@ -36,8 +36,17 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
+      this.setState({ productCount })
+      //Load products.
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
+      //console.log(productCount.toString())
       this.setState({ loading: false })
+      //console.log(this.state.products)
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
 
@@ -53,12 +62,21 @@ class App extends Component {
       loading: true
     }
     this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
 
   createProduct(name, price) {
     this.setState({ loading: true })
     this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
     .once('receipt', (receipt) => {
       this.setState({ loading: false })
     })
@@ -74,7 +92,9 @@ class App extends Component {
               { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
                 : <Main
-                createProduct={this.createProduct}/>
+                products={this.state.products}
+                createProduct={this.createProduct}
+                purchaseProduct={this.purchaseProduct}/>
               }
             </main>
           </div>
